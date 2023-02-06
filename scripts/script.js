3b5af7a0687ba2aa015ddc669919c015c25255a0
СтажерИТО <@lama.tomsk.ru>
2023-02-06 16:54:41 +0700

    add sidebar

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('.openbtn');
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
