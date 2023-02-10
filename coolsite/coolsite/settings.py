4257a2c2ee466fefaab571225fb0a4176693d79c
DmIToR <dmitor00@mail.ru>
2023-02-10 13:54:17 +0700

    Update html

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 132911,
+        'PASSWORD': 12345678,
         'HOST': 'localhost'
     }
 }
